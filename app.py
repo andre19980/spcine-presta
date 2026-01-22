989c70cb4dfa505ed5b963496d107178e389129f
André Solak <akasolak@gmail.com>
2026-01-22 11:39:05 -0300

    Revert "Add page gerador_conciliacao_bancaria"
    This reverts commit e8a4110185cdd3ebe3fd4605c350989c8414b7a7.

--- app.py
+++ app.py
@@ -2,21 +2,78 @@ import streamlit as st
 from openpyxl import load_workbook
 # from base64 import b64encode
 
-from pages.gerador_relatorio import gerador_relatorio
-from pages.gerador_conciliacao_bancaria import gerador_conciliacao_bancaria
+from layout.user_menu import user_menu
 from layout.login_screen import login_screen
+from layout.cabecalho import cabecalho
+from layout.conciliacao_bancaria import conciliacao_bancaria
+from layout.relacao_pagamentos import relacao_pagamentos
+from layout.demonstrativo_orcamentario import demonstrativo_orcamentario
+from layout.analise import analise
+from functions.utils import generate_pdf_from_dataframe
 
 st.set_page_config(
   page_title="SpcinePresta",
   layout="wide"
 )
 
-if not st.user.is_logged_in:
-  pg = st.navigation([st.Page(login_screen)])
-else:
-  pg = st.navigation([
-    st.Page(gerador_relatorio, title="Gerador de Relatório"),
-    st.Page(gerador_conciliacao_bancaria, title="Gerador de Conciliação Bancária")
-  ])
+def main_app():
+  uploaded_file = st.file_uploader("Escolha um arquivo .xlsx", type=["xlsx"])
 
-pg.run()
+  if uploaded_file is not None:
+    handle_file(uploaded_file)
+
+  return
+
+def handle_file(file):
+  wb = load_workbook(file, data_only=True)
+
+  st.divider()
+
+  with st.container(horizontal=True, horizontal_alignment='center'):
+    st.image("logo_spcine-principal.png", width=200)
+
+  # Cabeçalho
+  cabecalho_vars = cabecalho(wb, file)
+
+  # Conciliação Bancária
+  cb_elements, cb_vars = conciliacao_bancaria(wb, file)
+
+  st.divider()
+
+  # Relação de Pagamentos
+  rp_elements, rp_vars = relacao_pagamentos(wb, file)
+  df_rp = rp_elements.pop()
+
+  # Demonstrativo Orçamentário
+  do_elements, do_vars = demonstrativo_orcamentario(wb, file)
+  df_do = do_elements.pop()
+
+  st.divider()
+
+  # Análise e Cruzamentos
+  analise_dfs = [df_rp, df_do]
+  analise_vars = [*do_vars, *cb_vars, *rp_vars]
+  analise_elements = analise(analise_dfs, analise_vars)
+
+  elements=[*cb_elements, *rp_elements, *do_elements, *analise_elements, cabecalho_vars]
+  # Embed PDF to display it:
+  # base64_pdf = b64encode(generate_pdf_from_dataframe(elements)).decode("utf-8")
+  # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1200" height="700" type="application/pdf">'
+  # st.markdown(pdf_display, unsafe_allow_html=True)
+
+  # Add a download button:
+  st.space(size="large")
+  with st.container(horizontal=True, horizontal_alignment='center'):
+    st.download_button(
+      label="Download PDF",
+      data=generate_pdf_from_dataframe(elements),
+      file_name=f"{cabecalho_vars[0]}-spcine.pdf",
+      mime="application/pdf",
+      type="primary"
+    )
+
+if not st.user.is_logged_in:
+  login_screen()
+else:
+  user_menu()
+  main_app()
--- functions/get_range.py
+++ functions/get_range.py
@@ -20,32 +20,3 @@ def get_range(start_indicator_text, end_indicator_text, worksheet):
       break
 
   return interval
-
-def get_range_conciliacao_bancaria(start_indicator_text, end_indicator_text, worksheet):
-  interval = None
-  start_line = None
-  start_col = None
-  end_line = None
-  end_col = None
-
-  for row in worksheet.iter_rows():
-    for cell in row:
-      if cell.value is not None:
-        cell_value = str(cell.value).strip().lower()
-
-        if cell_value == start_indicator_text:
-          start_line = cell.row
-          start_col = cell.column
-
-        if start_line is not None and cell.row == start_line and cell_value == 'saldo':
-          end_col = cell.column
-
-        if cell_value == end_indicator_text:
-          end_line = cell.row
-          break
-      
-    if end_line is not None:
-      interval = [(start_line, start_col), (end_line, end_col)]
-      break
-
-  return interval
